a06e70514698a6c8b6a056b844c4ee24e8b77539
amar3344 <amarkumarreddynew@gmail.com>
2023-01-31 09:44:21 +0000

    given code

---
+++ src/components/Cart/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Cart = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Products = () => (
+  <div>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+    </div>
+  </div>
+)
+
+export default Products
